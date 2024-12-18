bf9be1c340ce6ebc568decd9d3452349d3729282
Austin Ryan Taylor <austinrtaylor91@gmail.com>
2024-12-18 09:30:05 -0500

    add initial dry ingredients logic -- need to type

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,16 +1,53 @@
-import { View, ScrollView } from 'react-native';
+import { useEffect, useState } from 'react';
+
+import { View, Text, FlatList } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 import TabHeader from '@/components/TabHeader';
+import { CONVERSIONS, MEASUREMENTS } from '@/constants/measurements';
 
 const DryIngredients = () => {
+  // eslint-disable-next-line
+  const [measurement, measurementSetter] = useState(MEASUREMENTS.dry[0]);
+  // eslint-disable-next-line
+  const [quantity, quantitySetter] = useState(1);
+  // eslint-disable-next-line
+  const [converted, convertedSetter] = useState([]);
+
+  // eslint-disable-next-line
+  const handleConversion = (measurement: string, qty: number) => {
+    // @ts-ignore:next-line
+    const conversion = CONVERSIONS.dry[measurement];
+    const convert = Object.keys(conversion).map((key) => ({
+      name: key,
+      qty: conversion[key] * qty,
+    }));
+    // @ts-ignore:next-line
+    convertedSetter(convert);
+  };
+  useEffect(() => {
+    handleConversion(measurement.abbr, quantity);
+  }, []);
+
   return (
     <SafeAreaView className="h-full bg-neutral-50">
-      <ScrollView contentContainerClassName="h-full">
-        <View className="min-h-[85vh] w-full p-4">
-          <TabHeader title="Dry Ingredients" />
-        </View>
-      </ScrollView>
+      <View className="min-h-[85vh] w-full p-4">
+        <TabHeader title="Dry Ingredients" />
+        <Text className="text-2xl text-primary-900">
+          {quantity} {measurement.name}
+        </Text>
+        <FlatList
+          data={converted}
+          renderItem={({ item }) => (
+            <View>
+              {/* @ts-ignore:next-line*/}
+              <Text>{item.qty}</Text>
+              {/* @ts-ignore:next-line*/}
+              <Text>{item.name}</Text>
+            </View>
+          )}
+        />
+      </View>
     </SafeAreaView>
   );
 };
